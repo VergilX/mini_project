b4a03081d5ffca436e96607fad1b452455a575a6
VergilX <abhinand.dmanoj@gmail.com>
2024-06-18 20:42:24 +0530

    DB configuration
    - Create schema
    - Functions: user create, delete

---
+++ db/db_actions.py
@@ -0,0 +1,84 @@
+from db_models import *
+from sqlmodel import Session, select
+
+# Encoding
+USER = 1
+DOCTOR = 2
+HOSPITAL = 3
+CHAT = 4
+DISEASE = 5
+
+
+def create(encoding, data):
+    """ Function to add entry to right table """
+
+    # No error checks as of now
+    with Session(engine) as session:
+        # create entity
+        if encoding == USER:
+
+            entity = User(**data)
+
+        elif encoding == DOCTOR:
+            entity = Doctor(**data)
+
+        elif encoding == HOSPITAL:
+            entity = Hospital(**data)
+
+        elif encoding == CHAT:
+            entity = Chat(**data)
+
+        elif encoding == DISEASE:
+            entity = Disease(**data)
+
+        # add to session and commit
+        session.add(entity)
+        session.commit()
+        print("Committed changes")
+
+
+def delete(encoding, key):
+    """ function to delete entry from right table """
+
+    # no error checks as of now
+    with session(engine) as session:
+        # fetch entity
+        if encoding == user:
+            statement = select(user).where(user.email == key)
+
+        elif encoding == doctor:
+            statement = select(doctor).where(doctor.name == key)
+
+        elif encoding == HOSPITAL:
+            statement = select(Hospital).where(Hospital.name == key)
+
+        elif encoding == CHAT:
+            statement = select(Chat).where(Chat.id == key)
+
+        elif encoding == DISEASE:
+            statement = select(Disease).where(Disease.name == key)
+
+        result = session.exec(statement)
+        entity = result.first()
+
+        # if found, delete
+        if entity is not None:
+            session.delete(entity)
+            session.commit()
+            print("Entity deleted successfully")
+        else:
+            print("No entry to delete")
+
+
+if __name__ == "__main__":
+    # Use only for testing purposes
+    print("Testing...")
+
+    data = dict(
+            name="Abhinand",
+            username="abhinand",
+            password="hashed_pwd",
+            email="luckyman@gmail.com",
+            district="Ernakulam"
+            )
+    delete(USER, key="luckyman@gmail.com")
---
+++ db/db_models.py
@@ -0,0 +1,87 @@
+from sqlmodel import Field, SQLModel, create_engine
+from pydantic import EmailStr
+
+
+class User(SQLModel, table=True):
+
+    # Set name of table
+    __tablename__ = "Users"
+
+    id: int | None = Field(default=None, primary_key=True)
+    name: str
+    username: str
+    password: str  # already hashed
+    email: EmailStr = Field(index=True)
+    district: str
+
+
+class Doctor(SQLModel, table=True):
+
+    __tablename__ = "Doctors"
+
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    hosp_id: int | None = Field(default=None, foreign_key="Hospitals.id")
+    specialisation_id: int | None = Field(default=None, foreign_key="Specialisation.id")
+    successful: int = 0
+
+
+class Hospital(SQLModel, table=True):
+
+    __tablename__ = "Hospitals"
+
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    district: str
+    location: str = ""
+    reputation: int | None = 0
+
+
+class Chat(SQLModel, table=True):
+    id: int | None = Field(default=None, index=True, primary_key=True)
+    user_id: int | None = Field(default=None, foreign_key="Users.id")
+    doctor_id: int | None = Field(default=None, foreign_key="Doctors.id")
+    message: str
+
+
+class Disease(SQLModel, table=True):
+
+    __tablename__ = "Diseases"
+
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(unique=True, index=True)
+    symptoms: str
+    causes: str
+    treatment: str
+
+
+class Specialisation(SQLModel, table=True):
+
+    __tablename__ = "Specialisation"
+
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+
+
+class Districts(SQLModel, table=True):
+
+    __tablename__ = "Districts"
+
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(unique=True, index=True)
+
+
+sqlite_file_name = "db.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+engine = create_engine(sqlite_url, echo=True)
+
+
+# Create tables
+def generate_schema():
+    SQLModel.metadata.create_all(engine)
+
+
+if __name__ == "__main__":
+    # Run file when you wanna create database
+    generate_schema()
